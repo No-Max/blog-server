026214c0e41d8e79994394470f6eee6b5a9e642a
Stanislav Lihodievski <grollii@mail.ru>
2021-08-19 20:00:27 +0300

    update and endpoints users and posts

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const express = require("express");
+const cors = require("cors");
+
+const posts = require("./models/posts.js");
+const users = require("./models/users.js")
+
 const app = express();
 const port = 3000;
-const posts = require("./models/posts.js");
-const cors = require("cors");
 
 app.use(cors());
 
 app.get("/posts", (req, res) => {
-  res.send(posts);
+  const postsWithUserNames = posts.map(post => {
+    return {
+      ...post,
+      userName: users.find(user => user.id === post.userId).name
+    }
+  })
+  res.send(postsWithUserNames);
 });
 
 app.get("/users", (req, res) => {
-  res.send([
-    {
-      id: 1,
-      name: "user1",
-    },
-    {
-      id: 2,
-      name: "user2",
-    },
-  ]);
+  res.send(users);
 });
 
 app.get("/posts/:id", (req, res) => {
